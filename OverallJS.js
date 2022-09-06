cc503e9ad3b1da2bdecfee2d79d3a0b4b7c4e3d7
Azreen731 <56175596+Azreen731@users.noreply.github.com>
2022-09-05 23:48:41 -0400

    linked js file and created icon for highlight

---
+++ OverallJS.js
@@ -0,0 +1,6 @@
+thing = document.getElementById("mexicoHighlight")
+thing2 = document.getElementById("mexico");
+thing.addEventListener("click", onClick)
+function onClick() {
+    thing2.style.backgroundColor = "coral";
+}
